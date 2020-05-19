f4f030baa89d5f52773a1e9f503f2dc971b23af9
Eric Settels <settels.eric@gmail.com>
2020-05-19 09:54:07 -0700

    add index and controller

---
+++ ToDoList/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+namespace ToDoList.Controllers
+{
+  public class HomeController : Controller
+  {
+
+    [Route("/")]
+    public ActionResult Index()
+    {
+      Item starterItem = new Item("Add first item to To Do List");
+      return View(starterItem);
+    }
+
+  }
+}
